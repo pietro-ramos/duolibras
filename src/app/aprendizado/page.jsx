6028055ecd5df7acbc74f3c05fa8c902121ddd0f
0Chaves <danieldossantoschaves@gmail.com>
2025-04-28 15:52:17 -0300

    Feat: desenvolvimento da página de aprensizado

--- src/app/aprendizado/page.jsx
+++ src/app/aprendizado/page.jsx
@@ -1,12 +1,19 @@
-
+import CardAprendizado from "@/components/cardAprendizado"
+import alfabeto from '@/assets/images/cardsAprendizado/alfabeto.png'
 
 export default function Aprendizado(){
     
     return(
-        <div className=" prose flex flex-col items-center text-center min-h-screen mx-auto p-4 mt-10">
-            <h1>Aprenda Libras</h1>
-            <h2>Página dedicada para expôr conteúdo de Libras</h2>
-            <p>Talvez podemos criar cards que redirecionam para tópicos como: alfabeto, números, frases do dia a dia, objetos escolares, objetos de casa, etc...</p>
+        <div className="flex flex-col items-center text-center min-h-screen">
+            <div className="prose mx-auto p-4 mt-10">
+                <h1>Aprenda Libras</h1>
+                <h2>Página dedicada para expôr conteúdo de Libras</h2>
+                <p>Talvez podemos criar cards que redirecionam para tópicos como: alfabeto, números, frases do dia a dia, objetos escolares, objetos de casa, etc...</p>
+            </div>
+            <div className="flex flex-row gap-1">
+                <CardAprendizado titulo='alfabeto' imagemUrl={alfabeto} descricao='Qualquer coisa so pra fazer se funcionou'/>
+                <CardAprendizado titulo='alfabeto' imagemUrl={alfabeto} descricao='Qualquer coisa so pra fazer se funcionou'/>
+            </div>
         </div>
-    )
+        )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import Link from "next/link"
 export default function Home() {
   return(
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-4 gap-1 w-screen">
-      <Link href='/jogos'><div className="card-menu">Jogos</div></Link>
-      <Link href='/aprendizado'><div className="card-menu">Aprendizado</div></Link>
-      <Link href='/historia'><div className="card-menu">Historia</div></Link>
+      <Link href='/jogos'><div className="card-menu card-hover">Jogos</div></Link>
+      <Link href='/aprendizado'><div className="card-menu card-hover">Aprendizado</div></Link>
+      <Link href='/historia'><div className="card-menu card-hover">Historia</div></Link>
     </main>
   )
 }
